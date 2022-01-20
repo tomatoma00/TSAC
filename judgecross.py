73473c348d3fbbbb930793f0e940a93e643c8baf
Shen Chentao <60208982+tomatoma00@users.noreply.github.com>
2022-01-20 19:07:06 +0800

    Update judgecross.py

--- judgecross.py
+++ judgecross.py
@@ -1,6 +1,12 @@
 # this file is to judge whether two edges cross
-
 import numpy as np
+
+ab = np.array([0, 0])
+ac = np.array([0, 0])
+ad = np.array([0, 0])
+cd = np.array([0, 0])
+ca = np.array([0, 0])
+cb = np.array([0, 0])
 
 
 def judge(line1a, line1b, line2c, line2d):
@@ -13,12 +19,12 @@ def judge(line1a, line1b, line2c, line2d):
     elif max(line2c[1], line2d[1]) <= min(line1a[1], line1b[1]):
         return 0
     else:
-        ab = np.array([line1b[0] - line1a[0], line1b[1] - line1a[1]])
-        ac = np.array([line2c[0] - line1a[0], line2c[1] - line1a[1]])
-        ad = np.array([line2d[0] - line1a[0], line2d[1] - line1a[1]])
-        cd = np.array([line2d[0] - line2c[0], line2d[1] - line2c[1]])
-        ca = np.array([line1a[0] - line2c[0], line1a[1] - line2c[1]])
-        cb = np.array([line1b[0] - line2c[0], line1b[1] - line2c[1]])
+        ab[0], ab[1] = line1b[0] - line1a[0], line1b[1] - line1a[1]
+        ac[0], ac[1] = line2c[0] - line1a[0], line2c[1] - line1a[1]
+        ad[0], ad[1] = line2d[0] - line1a[0], line2d[1] - line1a[1]
+        cd[0], cd[1] = line2d[0] - line2c[0], line2d[1] - line2c[1]
+        ca[0], ca[1] = line1a[0] - line2c[0], line1a[1] - line2c[1]
+        cb[0], cb[1] = line1b[0] - line2c[0], line1b[1] - line2c[1]
         result1 = np.dot(np.cross(ab, ac), np.cross(ab, ad))
         result2 = np.dot(np.cross(cd, ca), np.cross(cd, cb))
         if result2 < 0 and result1 < 0:
