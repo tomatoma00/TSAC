8e1dbbb8c1d7a2562510be1d5dc54eeb67ccdef5
Shen Chentao <60208982+tomatoma00@users.noreply.github.com>
2022-01-20 19:30:27 +0800

    Update judgecross.py

--- judgecross.py
+++ judgecross.py
@@ -1,13 +1,11 @@
-# this file is to judge whether two edges cross
 import numpy as np
 
-ab = np.array([0, 0])
-ac = np.array([0, 0])
-ad = np.array([0, 0])
-cd = np.array([0, 0])
-ca = np.array([0, 0])
-cb = np.array([0, 0])
-
+def cross(p1,p2,p3): 
+    x1=p2[0]-p1[0]
+    y1=p2[1]-p1[1]
+    x2=p3[0]-p1[0]
+    y2=p3[1]-p1[1]
+    return x1*y2-x2*y1
 
 def judge(line1a, line1b, line2c, line2d):
     if max(line1a[0], line1b[0]) <= min(line2c[0], line2d[0]):
@@ -19,14 +17,9 @@ def judge(line1a, line1b, line2c, line2d):
     elif max(line2c[1], line2d[1]) <= min(line1a[1], line1b[1]):
         return 0
     else:
-        ab[0], ab[1] = line1b[0] - line1a[0], line1b[1] - line1a[1]
-        ac[0], ac[1] = line2c[0] - line1a[0], line2c[1] - line1a[1]
-        ad[0], ad[1] = line2d[0] - line1a[0], line2d[1] - line1a[1]
-        cd[0], cd[1] = line2d[0] - line2c[0], line2d[1] - line2c[1]
-        ca[0], ca[1] = line1a[0] - line2c[0], line1a[1] - line2c[1]
-        cb[0], cb[1] = line1b[0] - line2c[0], line1b[1] - line2c[1]
-        result1 = np.dot(np.cross(ab, ac), np.cross(ab, ad))
-        result2 = np.dot(np.cross(cd, ca), np.cross(cd, cb))
+        
+        result1 = cross(line1a,line1b,line2c)*cross(line1a,line1b,line2d)
+        result2 = cross(line2c,line2d,line1a)*cross(line2c,line2d,line1b)
         if result2 < 0 and result1 < 0:
             return 1
         else:
